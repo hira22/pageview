5119e134e148db1ef361843228ccc3a6ca6a0548
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-03-03 15:42:17 +0900

    Initial Commit

---
+++ pageview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pageview
+//
+//  Created by hiraoka on 2020/03/03.
+//  Copyright © 2020 hiraoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
